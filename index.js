362f37d8a5fa014ef630a81aa0c2770bf4a06643
Wai Sze <waisze0212@yahoo.com>
2022-09-27 20:40:30 -0500

    commit tree 4b30f3670ddb792614eb8b11520c3c1031b2af16

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    
+    let messageArray = [];
+    
+    for (let i = 0; i < namesArray.length; i++) {
+        messageArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+
+    }
+    
+    return messageArray;
+
+}
+
+function countDown(positiveInteger) {
+    
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger);
+        positiveInteger--; 
+    }
+
+    
+
+}
